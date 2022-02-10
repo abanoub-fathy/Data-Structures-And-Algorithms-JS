815a4eef39a5038c59da309e91e372f3c504c0ca
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2022-02-10 17:56:10 +0200

    Adding another solution for fibonacci series using memoization in dynamic programming

--- 23- Dynamic Programming/fibonacci.js
+++ 23- Dynamic Programming/fibonacci.js
@@ -1,9 +1,22 @@
 const fib = (n) => {
-    // base case
-    if (n <= 2) return 1;
+  // base case
+  if (n <= 2) return 1;
 
-    // recursive case
-    return fib(n - 1) + fib(n - 2);
-}
+  // recursive case
+  return fib(n - 1) + fib(n - 2);
+};
 
-fib(10)
+fib(10); // big O ==> 2^N
+
+//////////////////////// Applying MemoIzation \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+const fibWithMemo = (n, memo = { 1: 1, 2: 1 }) => {
+  // base case
+  if (memo[n]) return memo[n];
+
+  // recursive case
+  let result = fibWithMemo(n - 1, memo) + fibWithMemo(n - 2, memo);
+  memo[n] = result;
+  return result;
+};
+
+fibWithMemo(10); // Big O ==> O(N)
